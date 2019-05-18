bd75d1524f4b742f0626f6bb88a82a4d462105ae
Kassam <mustaphakassam@gmail.com>
2019-05-18 11:00:09 +0300

    design the markup for the login

---
+++ application/views/login.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH')OR exit('Exit'); ?>
+<section>
+  <div class="">
+    <div class="col-md-4 container">
+      <form class="" action="index.html" method="post">
+        <div class="form-group">
+          <label for=""><i class="fa fa-at"></i></label>
+          <input placeholder="Email" class="form-control" type="text" name="" value="">
+        </div>
+        <div class="form-group">
+          <label for=""><i class="fa fa-lock"></i></label>
+          <input placeholder="Password" class="form-control" type="password" name="" value="">
+        </div>
+        <div class="form-group">
+          <button type="submit" name="button">LOGIN</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
