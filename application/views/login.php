0ac4cd1378324868a54ad88dca356574d4b298f4
Kassam <mustaphakassam@gmail.com>
2019-05-18 11:46:57 +0300

    complete the login page

--- application/views/login.php
+++ application/views/login.php
@@ -2,19 +2,35 @@
 <section>
   <div class="">
     <div class="col-md-4 container">
-      <form class="" action="index.html" method="post">
-        <div class="form-group">
-          <label for=""><i class="fa fa-at"></i></label>
-          <input placeholder="Email" class="form-control" type="text" name="" value="">
-        </div>
-        <div class="form-group">
-          <label for=""><i class="fa fa-lock"></i></label>
-          <input placeholder="Password" class="form-control" type="password" name="" value="">
-        </div>
-        <div class="form-group">
-          <button type="submit" name="button">LOGIN</button>
-        </div>
-      </form>
+      <div class="des-form-holder">
+        <h5>LOGIN</h5>
+        <form class="" action="index.html" method="post">
+          <div class="form-group">
+            <div class="row">
+              <div class="col-md-1">
+                <label for=""><i class="fa fa-at"></i></label>
+              </div>
+              <div class="col-md-11">
+                <input placeholder="Email" class="des-input" type="text" name="" value="">
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="col-md-1">
+                <label for=""><i class="fa fa-lock"></i></label>
+              </div>
+              <div class="col-md-11">
+                <input placeholder="Password" class="des-input" type="password" name="" value="">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 container">
+            <button class="col-md-12 btn btn-primary des-btn" type="submit" name="button">LOGIN</button>
+            <p>New user, <a href="#">Create account</a> </p>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </section>
