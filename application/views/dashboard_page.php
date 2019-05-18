e3e205ab1f6b5829f797d9980ecc8c46cf875771
Kassam <mustaphakassam@gmail.com>
2019-05-18 13:26:56 +0300

    customizing the shadow of the dashboad

--- application/views/dashboard_page.php
+++ application/views/dashboard_page.php
@@ -1,11 +1,32 @@
 <?php defined('BASEPATH')OR exit('Exit'); ?>
 <section>
   <div class="container-fluid des-profile-holder">
-    <div class="des-profile-bar">
-      <ul>
-        <li>Fatuma Mussa</li>
-        <li><a href="#"> <i class="fa fa-power-off"></i> </a></li>
-      </ul>
+    <div class="row">
+      <div class="col-md-2 des-logo"> <h4><i class="fa fa-medkit"></i> <span>DES</span> </h4> </div>
+      <div class="col-md-10 des-for-shadow">
+        <div class="des-profile-bar">
+          <ul>
+            <li>Fatuma Mussa</li>
+            <li><a href="#"> <i class="fa fa-power-off"></i> </a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-2 des-nav-bar">
+        <ul class="des-nav">
+          <li> <a href="#">Enter data</a> </li>
+          <li> <a href="#">Enter data</a> </li>
+          <li> <a href="#">Enter data</a> </li>
+          <li> <a href="#">Enter data</a> </li>
+          <li> <a href="#">Enter data</a> </li>
+        </ul>
+      </div>
+      <div class="col-md-10 des-content-bar">
+
+      </div>
     </div>
   </div>
 </section>
