f31b04b38d2df0eabc2a187acd7aa39510b3cad7
Kassam <mustaphakassam@gmail.com>
2019-05-19 11:58:40 +0300

    one file for profile bar for the user

--- application/views/dashboard_page.php
+++ application/views/dashboard_page.php
@@ -1,31 +1,15 @@
 <?php defined('BASEPATH')OR exit('Exit'); ?>
 <section>
   <div class="container-fluid des-profile-holder">
-    <div class="row">
-      <div class="col-md-2 des-logo"> <h4><i class="fa fa-medkit"></i> <span>DES</span> </h4> </div>
-      <div class="col-md-10 des-for-shadow">
-        <div class="des-profile-bar">
-          <ul>
-            <li>
-              <span>Fatuma Mussa</span>
-              <label>Doctor</label>
-            </li>
-            <li><a href="#"> <i class="fa fa-power-off"></i> </a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <!-- profile bar here -->
+    <?php $this->load->view('profile_bar');?>
+    <!-- profile bar end here -->
   </div>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-2 des-nav-bar">
-        <ul class="des-nav">
-          <li> <a href="#">Enter Patient</a> </li>
-          <li> <a href="#">Patient List</a> </li>
-          <li> <a href="#">Enter Doctor</a> </li>
-          <li> <a href="#">Doctor List</a> </li>
-        </ul>
-      </div>
+      <!-- profile bar here -->
+      <?php $this->load->view('nav');?>
+      <!-- profile bar end here -->
       <div class="col-md-12 des-content-bar">
         <div class="row">
           <div class="col-md-2"></div>
---
+++ application/views/profile_bar.php
@@ -0,0 +1,14 @@
+<div class="row">
+  <div class="col-md-2 des-logo"> <h4><i class="fa fa-medkit"></i> <span>DES</span> </h4> </div>
+  <div class="col-md-10 des-for-shadow">
+    <div class="des-profile-bar">
+      <ul>
+        <li>
+          <span>Fatuma Mussa</span>
+          <label>Doctor</label>
+        </li>
+        <li><a href="#"> <i class="fa fa-power-off"></i> </a></li>
+      </ul>
+    </div>
+  </div>
+</div>
