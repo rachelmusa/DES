5bfe1e9d551278415ecf3385fe5ecc19f51b02b4
Kassam <mustaphakassam@gmail.com>
2019-05-19 21:32:16 +0300

    update

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -7,8 +7,10 @@ class UserModel extends CI_model
   function login($email, $password)
   {
     $data = array('email' =>$email , 'password'=>$password);
-    $this->db->select('*');
-    $this->db->from('users');
+    $this->db->select('u.fname,u.lname,u.mobile,u.email,j.name as jobname,h.name as hospitalname');
+    $this->db->from('users as u');
+    $this->db->join('jobtype as j','j.id = u.jobtypeid','inner');
+    $this->db->join('hospitals as h','h.id = u.hospitalsid','inner');
     $this->db->where($data);
     $result = $this->db->get();
     if ($result) {
